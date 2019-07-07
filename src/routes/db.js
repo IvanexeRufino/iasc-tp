9b536d188eed8cc808a805e7be3e2aa0f3008d15
Federico Perez <FPerez@coasa.com.ar>
2019-07-07 15:33:37 -0300

    Agrego una skiÂplist para almacenar los valores. Dejo solo get y put.

--- src/routes/db.js
+++ src/routes/db.js
@@ -1,7 +1,34 @@
 import { Router } from 'express'
 const router = Router();
 
+var SkipList = require("dsjslib/lib/SkipList");//.SkipList;
+var list = new SkipList();
 
+router.get('/', async(req,res) => {
+
+    console.log(JSON.stringify(req.query));
+    let resp = {};
+    if(req.query.method === "equal"){
+        resp = handleGetEqual(req.query);
+    }
+    else if(req.query.method === "gt"){
+        resp = handleComparison(req.query,(a,b)=>{return a>b;})
+    }
+    else if(req.query.method === "lt"){
+        resp = handleComparison(req.query,(a,b)=>{return a<b;})
+    }else{
+        resp = {error: -1, message: 'Invalid query param. Must be: ?key=unaKey;method=equal/gt/lt'};
+    }
+
+    res.json(resp);
+});
+
+router.put('/', async(req,res) => {
+    list.put(req.query.key,req.query.value);
+    res.json({result: "OK"});
+});
+
+/*
 router.get('/', async (req, res) => {
     res.json({
         message: `Ingrese una clave como parametro de ruta.`
@@ -17,12 +44,13 @@ router.post('/:key', async (req, res) => {
 });
 
 router.get('/:key', async (req, res) => {
-    const { key } = req.params;
-    res.json(key);
+    const { paramKey } = req.params;
+    handleGetEqual({key:paramKey,res});
 });
 
 router.delete('/:key', async (req, res) => {
     const { key } = req.params;
+
     res.json({
         message: `Clave ${key} borrada exitosamente.`
     });
@@ -34,6 +62,31 @@ router.put('/:key', async (req, res) => {
     res.json({
         message: `Clave ${key} actualizada con el valor ${value} exitosamente.`
     });
-});
+});*/
+
+function handleGetEqual(body){
+    let resp = list.get(body.key);
+    if(resp != null){
+        return resp;
+    }else{
+        return {
+            error: -1,
+            message: "Could not find the key supplied"
+        };
+    }
+}
+
+function handleComparison(body,res,comp){
+    let set = list.entrySet();
+    let resp = [];
+    set.forEach(e => {
+        if(comp(e.key,body.key)){
+            resp.push(e);
+        }
+    });
+
+    return resp;
+}
+
 
 export default router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 import { Router } from 'express'
 const router = Router();
 
-router.get('/', (req, res) => {
+/*router.get('/', (req, res) => {
     res.send('Bienvenido a IACS Key-Value DB');
-});
+});*/
 
 export default router;
