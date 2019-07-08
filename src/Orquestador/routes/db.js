d12412f16da873cc12c14f6ad1fbd29b61ecf432
Federico Perez <FPerez@coasa.com.ar>
2019-07-08 16:12:31 -0300

    Quito el metodo post. Valido que la request sea correcta antes de enviarla al nodo de datos

--- src/Orquestador/routes/db.js
+++ src/Orquestador/routes/db.js
@@ -2,8 +2,6 @@ import { Router } from 'express'
 import { Socket } from 'net';
 
 const router = Router();
-var SkipList = require("dsjslib/lib/SkipList");//.SkipList;
-var list = new SkipList();
 
 var net = require('net');
 
@@ -13,6 +11,7 @@ function sendRequestToDataNode(msg,res){
     socket.on('data', (chunk) => {
         console.log('data recieved: ' + chunk);
         res.send(chunk);
+        socket.close();
     });
     socket.json(msg);
 }
@@ -33,133 +32,40 @@ function getSocket(msg, res){
 }
 
 router.get('/:key', async(req,res) => {
-
     console.log(JSON.stringify(req.query));
 
-    sendRequestToDataNode({
-        operation: "GET",
-        key: req.params.key,
-        method: req.query.method,
-    }, res);
-
-    /*
-    let method = req.query.method;
-    let key = req.params.key;
-    let resp = {};
-    switch(method) {
-        case "equal":
-            resp = handleGetEqual(key);
-            break;
-        case "gt":
-            resp = handleComparison(key,(a,b)=>{return a>b;});
-            break;
-        case "lt":
-            resp = handleComparison(key,(a,b)=>{return a<b;});
-            break;
-        default:
-            resp = {error: 400, message: 'Bad request, specify a valid method as a queryparam'};
-            break;
+    if( req.query.method === undefined || 
+        (req.query.method !== "equal" && req.query.method !== "gt" 
+        && req.query.method !== "lt" ) 
+    ){
+        res.json({error: 400, message: 'Bad request, specify a valid method as a queryparam'});
+    }else {
+        sendRequestToDataNode({
+            operation: "GET",
+            key: req.params.key,
+            method: req.query.method,
+        }, res);
     }
-    res.json(resp);*/
 });
-
-/*
-router.post('/:key', async(req,res) => {
-    let value = req.body.value;
-    let keyToFound = req.params.key;
-    let resp = {};
-
-    if(value !== undefined) {
-        if(!valueAlreadyExists(keyToFound)) {
-            list.put(keyToFound, value);
-
-            resp = {result: "OK"};
-        } else {
-            resp = {error: 400, message: 'Bad request, Key already exists if you want to edit it use PUT'};
-        }
-    } else {
-        resp = {error: 400, message: 'Bad request, Post a json with value property on it'};
-    }
-
-    res.json(resp);
-});*/
 
 router.put('/:key', async(req,res) => {
 
-    sendRequestToDataNode({
+    if( req.body.value === undefined){
+        res.json({error: 400, message: 'Bad request, Post a json with value property on it'});
+    }else{
+        sendRequestToDataNode({
             operation: "PUT",
             key: req.params.key,
             value: req.body.value
         }, res);
-
-    /*
-    let value = req.body.value;
-    let keyToFound = req.params.key;
-    let resp = {};
-
-    if(value !== undefined) {
-        if(valueAlreadyExists(keyToFound)) {
-            list.put(keyToFound,value);
-
-            resp = {result: "OK"};
-        } else {
-            resp = {error: 400, message: 'Bad request, Key doesnt exist use POST instead'};
-        }
-    } else {
-        resp = {error: 400, message: 'Bad request, Post a json with value property on it'};
     }
-
-    res.json(resp);*/
 });
 
 router.delete('/:key', async(req,res) => {
-
     sendRequestToDataNode({
         operation: "DELETE",
         key: req.params.key
     }, res);
-
-    /*
-    let resp = {};
-    let keyToFound = req.params.key;
-
-    if(valueAlreadyExists(keyToFound)) {
-        list.put(keyToFound,value);
-
-        resp = {result: "OK"};
-    } else {
-        resp = {error: 400, message: 'Bad request, Key doesnt exist'};
-    }
-
-    res.json(resp);*/
 });
-
-function handleGetEqual(key){
-    let resp = list.get(key);
-    if(resp != null){
-        return resp;
-    }else{
-        return {
-            error: 404,
-            message: 'Could not find the key supplied'
-        };
-    }
-}
-
-function handleComparison(keyToFound,comp){
-    let set = list.entrySet();
-    let resp = [];
-    set.forEach(e => {
-        if(comp(e.key,keyToFound)){
-            resp.push(e.key);
-        }
-    });
-
-    return resp;
-}
-
-function valueAlreadyExists(key) {
-    return list.get(key) !== undefined;
-}
 
 export default router;
