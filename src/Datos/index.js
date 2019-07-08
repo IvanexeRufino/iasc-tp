f7466635dc498c82a7303fc59b1f6eadae7007fe
Federico Perez <FPerez@coasa.com.ar>
2019-07-08 02:15:05 -0300

    Ahora el orquestador llama al nodo de datos a traves de sockets.
    - Es un mock, solo crea una conexion hacia el otro nodo y envia el request. Posteriormente debería elegir a cual de todos
    llamar.
    - Falta agregar los metodos que agrego Ivan en el nodo de datos.

--- src/Datos/index.js
+++ src/Datos/index.js
@@ -5,5 +5,5 @@ dataServer.on('error', (err) => {
     console.log(JSON.stringify(err));
 })
 
-dataServer.listen(23405,'127.0.0.1');
+dataServer.listen(12345,'127.0.0.1');
 console.log('listening on port 23405');
--- src/Orquestador/routes/db.js
+++ src/Orquestador/routes/db.js
@@ -1,12 +1,48 @@
 import { Router } from 'express'
+import { Socket } from 'net';
 
 const router = Router();
 var SkipList = require("dsjslib/lib/SkipList");//.SkipList;
 var list = new SkipList();
 
+var net = require('net');
+
+function sendRequestToDataNode(msg,res){
+    let socket = getSocket(msg, res);
+    //Setteo un handler que responda este request REST
+    socket.on('data', (chunk) => {
+        console.log('data recieved: ' + chunk);
+        res.send(chunk);
+    });
+    socket.json(msg);
+}
+
+//Aca tenemos que decidir a que socket le vamos a pasar el request
+function getSocket(msg, res){
+    //Por ahora solo devolvemos un socket recien conectado
+    let socket = new Socket();
+    socket.json = function(data){
+        console.log('Sending data: ' + JSON.stringify(data));
+        socket.write(JSON.stringify(data));
+    }
+    socket.connect(12345,'127.0.0.1',() =>{
+        console.log("Connect to client Node Successful");
+    });
+
+    return socket;
+}
+
 router.get('/:key', async(req,res) => {
 
     console.log(JSON.stringify(req.query));
+
+    sendRequestToDataNode({
+        operation: "GET",
+        key: req.params.key,
+        method: req.query.method,
+    }, res);
+
+    /*
     let method = req.query.method;
     let key = req.params.key;
     let resp = {};
@@ -24,9 +60,10 @@ router.get('/:key', async(req,res) => {
             resp = {error: 400, message: 'Bad request, specify a valid method as a queryparam'};
             break;
     }
-    res.json(resp);
+    res.json(resp);*/
 });
 
+/*
 router.post('/:key', async(req,res) => {
     let value = req.body.value;
     let keyToFound = req.params.key;
@@ -45,9 +82,17 @@ router.post('/:key', async(req,res) => {
     }
 
     res.json(resp);
-});
+});*/
 
-router.put('/', async(req,res) => {
+router.put('/:key', async(req,res) => {
+
+    sendRequestToDataNode({
+            operation: "PUT",
+            key: req.params.key,
+            value: req.body.value
+        }, res);
+
+    /*
     let value = req.body.value;
     let keyToFound = req.params.key;
     let resp = {};
@@ -64,10 +109,17 @@ router.put('/', async(req,res) => {
         resp = {error: 400, message: 'Bad request, Post a json with value property on it'};
     }
 
-    res.json(resp);
+    res.json(resp);*/
 });
 
 router.delete('/:key', async(req,res) => {
+
+    sendRequestToDataNode({
+        operation: "DELETE",
+        key: req.params.key
+    }, res);
+
+    /*
     let resp = {};
     let keyToFound = req.params.key;
 
@@ -79,7 +131,7 @@ router.delete('/:key', async(req,res) => {
         resp = {error: 400, message: 'Bad request, Key doesnt exist'};
     }
 
-    res.json(resp);
+    res.json(resp);*/
 });
 
 function handleGetEqual(key){
