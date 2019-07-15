7bc8ea2ad8ade43010282df69c0d6944cd391910
fedelway <fede21perez@hotmail.com>
2019-07-14 22:03:00 -0300

    Corrijo error al levantar nodo datos. -Debe buscar los puertos en los replicaSets del config, no en el array de nodos.

--- src/Datos/index.js
+++ src/Datos/index.js
@@ -6,11 +6,16 @@ const config = require("../Orquestador/config.hjson");
 dataServer.on('error', console.log);
 
 let port = process.argv[2];
-const start = config.Datos[0].Port;
-const end = config.Datos[config.Datos.length - 1].Port;
-if (!port || port > end || port < start) {
-    console.error(`You must specify a port number between ${start} and ${end} as a parameter.`);
-    process.exit();
+let correctPorts = [];
+
+for(let rs of config.ReplicaSets){
+    for(let nodo of rs.Set){
+        correctPorts.push(nodo.Port);
+    }
+}
+
+if(!correctPorts.includes(port)){
+    console.error('Yoy must specify one of the following ports: (' + correctPorts.reduce( (p1,p2) => p1 + ',' + p2) + ')');
 }
 
 dataServer.listen(port);
