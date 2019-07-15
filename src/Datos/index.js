aaa584b69734164abf56efd6fe8ce70e92d44aab
Juan Pablo Bulbulian <jpbulbulian@gmail.com>
2019-07-15 01:53:30 -0300

    Agrego toString() al numero de port para la comparación

--- src/Datos/index.js
+++ src/Datos/index.js
@@ -3,20 +3,18 @@ import dataServer from './dataServer';
 require("hjson/lib/require-config");
 const config = require("../Orquestador/config.hjson");
 
-dataServer.on('error', console.log);
-
-let port = process.argv[2];
+const port = process.argv[2];
 let correctPorts = [];
 
-for(let rs of config.ReplicaSets){
-    for(let nodo of rs.Set){
-        correctPorts.push(nodo.Port);
-    }
+for (let rs of config.ReplicaSets)
+    for (let nodo of rs.Set)
+        correctPorts.push(nodo.Port.toString());
+
+if (!correctPorts.includes(port)) {
+    console.error(`Yoy must specify one of the following ports: ${correctPorts.toString()}`);
+    process.exit();
 }
 
-if(!correctPorts.includes(port)){
-    console.error('Yoy must specify one of the following ports: (' + correctPorts.reduce( (p1,p2) => p1 + ',' + p2) + ')');
-}
-
+dataServer.on('error', console.error);
 dataServer.listen(port);
 console.log('Listening on port:', port);
